54c829c7fd700185373ca7901f17110b58e33251
Vilém Jeniš <vilemj@kentico.com>
2018-07-23 16:58:19 +0200

    Show how react mounts and how to write JSX.

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-import { App } from './App';
+const theThingIWantToHaveRendered = (
+  <div className="a-random-classname">
+    <h2>Hey a title!!!</h2>
+    <p>
+      A paragraph with text. How coole is that?
+    </p>
+    <p
+      style={{
+        textAlign: 'center',
+        color: 'red',
+      }}
+    >
+      And another one. Nothing can stop me. I'm on a roll.
+    </p>
+    <p align="center">
+      Nothing but my lack of inspiration for making up unnecessarily personal lorem ipsum.
+    </p>
+  </div>
+);
 
-ReactDOM.render(<App />, document.getElementById('app-root'));
+ReactDOM.render(theThingIWantToHaveRendered, document.getElementById('app-root'));
--- webpack.config.js
+++ webpack.config.js
@@ -8,30 +8,68 @@ const mode = process.env.NODE_ENV || 'development';
 module.exports = {
   entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, '/build'),
+    path: path.resolve('./build'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
-      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: [
-        'babel-loader',
-        'eslint-loader'
-      ] },
-      { test: /\.(ts|tsx)$/, exclude: /node_modules/, use: [
-        'ts-loader',
-        { loader: 'tslint-loader', options: { configFile: 'tslint.json' } }
-      ] },
-      { test: /\.css$/, use: [
-        { loader: 'file-loader', options: { name: 'styles/[name].[ext]' } },
-        { loader: 'extract-loader', options: { publicPath: '../' } },
-        { loader: 'css-loader' }
-      ] },
-      { test: /\.(eot|svg|ttf|woff|woff2)/, use: [
-        { loader: 'url-loader', options: { name: 'assets/[name].[ext]', limit: 10000 } }
-      ] },
-      { test: /\.(html|jpg|jpeg|png|ico|gif)/, use: [
-        { loader: 'file-loader', options: { name: '[path][name].[ext]', context: 'public' } }
-      ] }
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader'
+        ]
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'ts-loader',
+          {
+            loader: 'tslint-loader',
+            options: { configFile: 'tslint.json' }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'styles/[name].[ext]' }
+          },
+          {
+            loader: 'extract-loader',
+            options: { publicPath: '../' }
+          },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'assets/[name].[ext]',
+              limit: 10000
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(html|jpg|jpeg|png|ico|gif)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              context: 'public'
+            }
+          }
+        ]
+      }
     ]
   },
   resolve: {
@@ -42,7 +80,7 @@ module.exports = {
   ],
   devtool: mode === 'development' ? 'eval-source-map' : null,
   devServer: {
-    contentBase: path.resolve(__dirname, '/build'),
+    contentBase: path.resolve('./public'),
     port: 3000,
     open: true
   }
