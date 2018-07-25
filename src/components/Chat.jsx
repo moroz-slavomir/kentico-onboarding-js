578084bd44ed6cfdc758a7111632419fd9aef41d
Vilém Jeniš <vilemj@kentico.com>
2018-07-25 18:09:31 +0200

    Final state (without server)

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { BaseComponent } from './DemoChatComponentBase';
+import { MessageList } from './MessageList';
+import { MessageForm } from './MessageForm';
 
 export class Chat extends BaseComponent {
   constructor() {
-    super();
+    super(); // Must be called to properly build the base object and initialize its prototype.
 
     /*
      * Add and initialize new state properties in the style of:
      * this.state.newProp = newValue;
      */
 
-    this.state.formState = {
-      nickName: '',
-      messageText: '',
-    };
+    this.state.nickName = '';
+    this.state.messageText = '';
   }
 
-  componentDidMount() {
-    this.postMessage('random name', 'random message');
-  }
+  sendMessage = () => {
+    this.postMessage(this.state.nickName, this.state.messageText);
+    this.setState({ messageText: '' });
+  };
 
   render() {
-    /**
+    /*
      * Render your list of messages and message form here
      * this.state has the shape of
      * {
@@ -31,8 +32,19 @@ export class Chat extends BaseComponent {
      *     text: string,
      *   }>,
      *   ... Whatever you add to it.
-     **/
+     */
 
-    return null;
+    return (
+      <div className="chat">
+        <MessageList messages={this.state.messages} />
+        <MessageForm
+          onSend={this.sendMessage}
+          nick={this.state.nickName}
+          onNickChange={(newNick) => this.setState({ nickName: newNick })}
+          message={this.state.messageText}
+          onMessageChange={newMessage => this.setState({ messageText: newMessage })}
+        />
+      </div>
+    );
   }
 }
---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class MessageForm extends React.Component {
+  static propTypes = {
+    onNickChange: PropTypes.func.isRequired,
+    onMessageChange: PropTypes.func.isRequired,
+    onSend: PropTypes.func.isRequired,
+    nick: PropTypes.string.isRequired,
+    message: PropTypes.string.isRequired,
+  };
+
+  onNickChange = (event) => {
+    this.props.onNickChange(event.target.value);
+  };
+  onMessageChange = (event) => {
+    this.props.onMessageChange(event.target.value);
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSend();
+  };
+
+  render() {
+    return (
+      <form
+        className="message-form"
+        onSubmit={this.onSubmit}
+      >
+        <label
+          htmlFor="nick"
+          className="message-form__nick-label"
+        >
+          Nick:
+        </label>
+        <input
+          className="message-form__nick-input"
+          name="nick"
+          value={this.props.nick}
+          onChange={this.onNickChange}
+        />
+        <label
+          className="message-form__message-label"
+          htmlFor="message"
+        >
+          Message:
+        </label>
+        <input
+          name="message"
+          className="message-form__message-input"
+          value={this.props.message}
+          onChange={this.onMessageChange}
+        />
+        <button type="submit">Send</button>
+      </form>
+    );
+  }
+}
